f8cd7ac0a9ca46284600d84de954480ca2a81762
David Monarrez <davidmonarrez3@gmail.com>
2019-04-26 11:51:37 -0700

    still working on form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,47 +1,17 @@
 import React from 'react';
 
-class SearchForm extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      resultsVisible: false
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(){
-    this.setState({resultsVisible: true});
-    console.log('resultsVisible is currently set to:' + this.state.formVisibleOnPage);
-  }
-
-  render() {
-    return (
-      <div>
-        <style jsx>{`
-          input {
-            width: 560px;
-            height: 40;
-            borderRadius: 15;
-            borderWidth: 0;
-            padding: 22px 0px 22px 24px;
-          }
-
-          button {
-            height: 40;
-            width: 40;
-          }
-        `}</style>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type='text'
-            id='search'
-            placeholder='Enter an address, neightborhood, city, or ZIP code'/>
-            <button type='submit'>Search</button>
-        </form>
-      </div>
-    ); // end of return
-  } //end of render
+function SearchForm(){
+  return (
+    <div>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          id='search'
+          placeholder='Enter an address, neightborhood, city, or ZIP code'/>
+          <button type='submit'>Search</button>
+      </form>
+    </div>
+  );
 }
 
 export default SearchForm;
