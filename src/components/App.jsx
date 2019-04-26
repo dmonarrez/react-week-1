f3cf4fd2789df2cc7dbc17f519371f024f302eec
David Monarrez <davidmonarrez3@gmail.com>
2019-04-26 14:35:32 -0700

    working on data binding and conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,21 @@ import Error404 from './Error404';
 import Home from './Home';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterHouseList: []
+    }
+    this.handleAddingSearchToList = this.handleAddingSearchToList.bind(this);
+  }
+
+  handleAddingSearchToList(newSearch) {
+    var newMasterHouseList = this.state.masterHouseList.slice();
+    newMasterHouseList.push(newSearch);
+    this.setState({masterHouseList: newMasterHouseList});
+  }
+
   componentDidMount() {
     document.title = "Zillow";
   }
@@ -17,7 +32,7 @@ class App extends React.Component {
         <Nav/>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route exact path='/newticket' component={NewTicketForm} /> //this needs to change to the page that will display the output of the search
+          <Route exact path='/newticket' render={()=><SearchControler onSearchOutput={this.handleAddingSearchToList} /> //this needs to change to the page that will display the output of the search
           <Route component={Error404}/>
         </Switch>
         <h1 style={{height: '50px'}}></h1>
