ca24089952f4cba5565810637c4db291ebfb1c4f
David Monarrez <davidmonarrez3@gmail.com>
2019-04-26 09:33:42 -0700

    made app component a stateful class and changed the title of the application to zillow from help queue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import TicketList from './TicketList';
 import Nav from './Nav';
 import { Switch, Route } from 'react-router-dom';
@@ -6,18 +7,23 @@ import NewTicketForm from './NewTicketForm';
 import Error404 from './Error404';
 import Home from './Home';
 
-function App(){
-  return (
-    <div>
-      <Nav/>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/newticket' component={NewTicketForm} />
-        <Route component={Error404}/>
-      </Switch>
-      <h1 style={{height: '50px'}}></h1>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    document.title = "Zillow";
+  }
+  render() {    
+    return (
+      <div>
+        <Nav/>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/newticket' component={NewTicketForm} />
+          <Route component={Error404}/>
+        </Switch>
+        <h1 style={{height: '50px'}}></h1>
+      </div>
+    );
+  }
 }
 
 export default App;
